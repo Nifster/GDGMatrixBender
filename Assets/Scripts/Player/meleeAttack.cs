4445d7f2194b17a19241751bd34a6a61b4059052
Chan Jing Hui Benedict <chanjhben@hotmail.com>
2016-10-13 20:54:33 +0800

    Added meleeAttack script for player. Not working as no prefabs for melee attack

---
+++ Assets/Scripts/Player/meleeAttack.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class meleeAttack : MonoBehaviour {
+
+	public GameObject meleeAttackPrefab;
+
+	private double coolDown = -0.1f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Mouse1)) {
+			if (coolDown < 0.0) {
+				coolDown = 2.0f;
+				//Instantiate melee attack prefab
+				Debug.Log("Haha");
+			}
+		}
+
+		if (coolDown > 0.0f) {
+			Debug.Log("Hehe");
+			coolDown -= Time.deltaTime;
+		}
+	}
+
+}
---
+++ Assets/Scripts/Player/meleeAttackTimer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class meleeAttackTimer : MonoBehaviour {
+
+	private float timer = 0.3f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer < 0.3f)
+			Destroy(this.gameObject);
+		else
+			timer -= Time.deltaTime;
+	}
+}
