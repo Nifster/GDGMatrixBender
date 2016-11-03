594ffebfd544759f7359c13f776608a3e627c2a5
Chan Jing Hui Benedict <chanjhben@hotmail.com>
2016-11-03 20:06:29 +0800

    Need to split bullet out into a single class by itself. Currently only the newly instantiated bullets will move in the opposite direction.

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -14,6 +14,10 @@ public class Enemy : MonoBehaviour
 
     public float LOSdistance;
 
+	private bool isReversed = false;
+	private float reverseTimer = 0.0f;
+	private float reverseCooldown = 0.0f;
+
     public GameObject bullet;
     public GameObject muzzle;
     public float bulletSpeed;
@@ -42,6 +46,22 @@ public class Enemy : MonoBehaviour
         //Get distance to player
         Vector2 playerDist = (playerObj.transform.position - transform.position);
         //Debug.Log(playerDist.magnitude);
+
+		if (Input.GetKey(KeyCode.I) && reverseCooldown <= 0.0f) {
+			isReversed = true;
+			reverseTimer = 2.0f;
+			reverseCooldown = 5.0f;
+		}
+
+		if (reverseTimer > 0.0f) {
+			reverseTimer -= Time.deltaTime;
+		} else {
+			isReversed = false;
+		}
+
+		if (reverseCooldown > 0.0f) {
+			reverseCooldown -= Time.deltaTime;
+		}
 
         Vector2 velocity = Vector2.zero;
         if(!active)
@@ -114,7 +134,12 @@ public class Enemy : MonoBehaviour
                 GameObject newBullet = Instantiate(bullet);
                 newBullet.transform.rotation = muzzle.transform.rotation;
                 newBullet.transform.position = muzzle.transform.position;
-        newBullet.GetComponent<Rigidbody2D>().velocity = (playerObj.transform.position - this.transform.position).normalized * (bulletSpeed + speed);
+//        newBullet.GetComponent<Rigidbody2D>().velocity = (playerObj.transform.position - this.transform.position).normalized * (bulletSpeed + speed);
+		if (isReversed) {
+			newBullet.GetComponent<Rigidbody2D>().velocity = -(playerObj.transform.position - this.transform.position).normalized * (bulletSpeed + speed);
+		} else {
+			newBullet.GetComponent<Rigidbody2D>().velocity = (playerObj.transform.position - this.transform.position).normalized * (bulletSpeed + speed);
+		}
         //speed = 0;
     }
 }
